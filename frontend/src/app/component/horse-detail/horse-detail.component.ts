4cc85fe8ec673cfd567755ccaf7b5892ded9f475
Florian Freitag <e11908096@student.tuwien.ac.at>
2021-03-11 19:21:55 +0100

    [5] Improve the detail horse view in the frontend

--- frontend/src/app/component/horse-detail/horse-detail.component.ts
+++ frontend/src/app/component/horse-detail/horse-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Observable, EMPTY, forkJoin, of } from "rxjs";
 import { Horse } from "src/app/dto/horse";
 import { Sport } from "src/app/dto/sport";
 import { HorseService } from "src/app/service/horse.service";
+import { SportService } from "src/app/service/sport.service";
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-horse-detail",
@@ -19,24 +22,66 @@ export class HorseDetailComponent implements OnInit {
 
   constructor(
     private horseService: HorseService,
+    private sportService: SportService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.loadData();
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = params.get("id");
+      this.loadData(id);
+    });
   }
 
-  private loadData(): void {
-    const id = +this.route.snapshot.paramMap.get("id");
+  private loadData(id: any): void {
     this.error = false;
     this.errorMessage = "";
     this.horseService.getHorseById(id).subscribe(
       (horse: Horse) => {
         this.horse = horse;
+        this.loadDependencies();
       },
       (error) => {
         this.defaultServiceErrorHandling(error);
       }
+    );
+  }
+
+  /**
+   * Load all "Dependencies" of the current horse.
+   * For example the horse only saves the Id of its favorite sport but that
+   * number is useless for the user, so we need to make an extra request to
+   * get the sports name.
+   */
+  private loadDependencies(): void {
+    let dependencies = [];
+    if (this.horse.favoriteSportId !== null) {
+      dependencies.push(
+        this.sportService.getSportById(this.horse.favoriteSportId)
+      );
+    } else {
+      dependencies.push(of(null));
+    }
+
+    if (this.horse.motherId !== null) {
+      dependencies.push(this.horseService.getHorseById(this.horse.motherId));
+    } else {
+      dependencies.push(of(null));
+    }
+
+    if (this.horse.fatherId !== null) {
+      dependencies.push(this.horseService.getHorseById(this.horse.fatherId));
+    } else {
+      dependencies.push(of(null));
+    }
+
+    forkJoin(dependencies).subscribe(
+      (results) => {
+        this.sport = results[0] as Sport;
+        this.mother = results[1] as Horse;
+        this.father = results[2] as Horse;
+      },
+      (error) => this.defaultServiceErrorHandling(error)
     );
   }
 
