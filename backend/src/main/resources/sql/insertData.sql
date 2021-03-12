cf8d4946054c3e9bb1354ceba01ec05fa2077fae
Florian Freitag <e11908096@student.tuwien.ac.at>
2021-03-12 10:44:27 +0100

    [1] Fix SQL errors caused by foreign key releations.

--- backend/src/main/resources/sql/insertData.sql
+++ backend/src/main/resources/sql/insertData.sql
@@ -2,6 +2,13 @@
 -- the IDs are hardcoded to enable references between further test data
 -- negative IDs are used to not interfere with user-entered data and allow clean deletion of test data
 
+-- Remove all foreign keys
+UPDATE horse
+SET mother=null,
+    father=null,
+    favoriteSport=null
+WHERE id < 0;
+
 DELETE
 FROM horse
 where id < 0;
@@ -17,7 +24,8 @@ VALUES (-1, 'Polo', NULL)
 ;
 
 INSERT INTO horse (id, name, description, birthday, sex, favoriteSport, mother, father)
-VALUES (-1, 'Beatrice Horseman', 'Throughout BoJacks flashbacks, Beatrice was passive-aggressive, sardonic, neglectful, cynical, bitter, jaded, and verbally abusive. She was an overall atrocious mother. ',
+VALUES (-1, 'Beatrice Horseman',
+        'Throughout BoJacks flashbacks, Beatrice was passive-aggressive, sardonic, neglectful, cynical, bitter, jaded, and verbally abusive. She was an overall atrocious mother. ',
         '1938-2-1', 'female', -2, NULL, NULL)
      , (-2, 'Bojack Horseman', 'Failed filmstar.', '1999-8-7', 'male', -1, -1, NULL)
 ;
