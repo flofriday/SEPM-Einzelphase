213a54539bc75072770d868701abbf89e492dd09
Florian Freitag <e11908096@student.tuwien.ac.at>
2021-03-30 22:23:49 +0200

    [17] Redesign error-alerts to be consistent across the frontend

--- frontend/src/app/component/horse-tree/horse-tree.component.ts
+++ frontend/src/app/component/horse-tree/horse-tree.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, ParamMap, Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { HorseTree } from "src/app/dto/horsetree";
 import { HorseService } from "src/app/service/horse.service";
 
@@ -13,6 +14,8 @@ export class HorseTreeComponent implements OnInit {
   tree$: Observable<HorseTree>;
   depth = 3;
   id: number;
+  error = false;
+  errorMessage = "";
 
   constructor(
     private horseService: HorseService,
@@ -28,7 +31,12 @@ export class HorseTreeComponent implements OnInit {
     this.route.queryParams.subscribe((params) => {
       this.depth = params["depth"];
       if (this.depth == null) this.depth = 3;
-      this.tree$ = this.horseService.getTreeById(this.id, this.depth);
+      this.tree$ = this.horseService.getTreeById(this.id, this.depth).pipe(
+        catchError((err) => {
+          this.defaultServiceErrorHandling(err);
+          return throwError(err);
+        })
+      );
     });
   }
 
@@ -36,5 +44,19 @@ export class HorseTreeComponent implements OnInit {
     this.router.navigate([], {
       queryParams: { depth: this.depth },
     });
+  }
+
+  private defaultServiceErrorHandling(error: any) {
+    console.log(error);
+    this.error = true;
+    if (error.status === 0) {
+      // If status is 0, the backend is probably down
+      this.errorMessage = "The backend seems not to be reachable";
+    } else if (error.error.message === "No message available") {
+      // If no detailed error message is provided, fall back to the simple error name
+      this.errorMessage = error.error.error;
+    } else {
+      this.errorMessage = error.error.message;
+    }
   }
 }
